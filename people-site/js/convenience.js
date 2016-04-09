921c963fa0e6f88a3460fd6c5387104e7ec811a2
kaiketsuakul <luca_lanzone@libero.it>
2016-04-09 17:25:32 +0200

    Commit del codice su un nuovo branch per evitare guai.

---
+++ people-site/js/convenience.js
@@ -0,0 +1,58 @@
+/**
+ * Funzioni ausiliarie ulteriori
+ */
+var stoptokens = /[':]/g;
+
+//Costruttore di query
+function submitted_query() {
+	this.queryObject = {
+			"from": 0,		//da cambiare in runtime
+			"size": 10,
+			"_source": false,
+			"fields": ["title", "description", "url"],
+			"highlight": {
+				"fields": {
+					"html_text": {
+						"fragment_size" : 150,
+						"number_of_fragments" : 3,
+						"pre_tags" : ["<b>"],
+						"post_tags" : ["</b>"]
+					}
+				}
+			},
+			"query": match_withKeywords
+	}
+}
+
+function parseQueryString(parse_function, query_string, query_obj) {
+	parse_function(query_string, query_obj);
+}
+
+function setUpQuery(qstr, qobj) {
+	var cleanString = qstr.replace(stoptokens, "");
+	qobj.query.bool.must[0].multi_match.query = cleanString;
+}
+
+var askCategory = function(qstr, qobj) {
+	if (qstr.indexOf(":") < 0) {
+		qobj.query.bool.must.splice(1, 1);
+	}
+	else {
+		var category = qstr.split(":")[1];
+		qobj.query.bool.must[1].match.html_text = keywords[category];
+	}
+}
+
+var askExactName = function(qstr, qobj) {
+	if (qstr.indexOf("'") >= 0) {
+		var exactName = qstr.split("'")[1].split(" ");
+		
+		qobj.query.bool.filter = {
+				"multi_match" : {
+					"query" : exactName.join(" "),
+					"fields" : ["title", "description", "html_text"],
+					"type" : "phrase"
+				}
+		};
+	}
+}
--- people-site/js/keywords.js
+++ people-site/js/keywords.js
@@ -1,4 +1,11 @@
 
-var pallavolo_words = "palla muro gioco campo squadra palleggio rete pallone giocatore passaggio";
-
+var pallavolo_words = "palla muro gioco campo squadra rete avversario passaggio attacco bagher";
 var pallavolo_trigger = ["pallavolo", "volley", "volleyball", "pallavolista"]
+
+var marina_words = "monitoraggio superiori salvataggio corpi materiali attrezzature remiero acqua porti minaccia";
+var marina_trigger = ["marina"]
+
+var keywords = {
+		"pallavolo" : "palla muro gioco campo squadra rete avversario passaggio attacco bagher",
+		"marina" : "monitoraggio superiori salvataggio corpi materiali attrezzature remiero acqua porti minaccia"
+}
